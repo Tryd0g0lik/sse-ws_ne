c68af462cf9a65e48cb7e3c5caaa01fe2cb0593a
Tryd0g0lik <work80@mail.ru>
2023-10-27 18:16:32 +0700

    src\app\backend\webpack.config.js: src\app\backend\tsconfig.json: update out

--- src/app/backend/src/serve/index.ts
+++ src/app/backend/src/serve/index.ts
@@ -11,15 +11,15 @@ const WS = require('ws');
 const app = new Koa();
 const server = http.createServer(app.callback);
 const wss = new WS.Server({ server });
-const websockets = require('websockets');
+const wsServer = require('wsServer');
 // app.use(async (ctx: any) => {
 // 	ctx.body = 'Hello World';
 // });
 
 
 
-console.log('[websockets]: ', websockets);
-websockets(wss);
+console.log('[wsServer]: ', wsServer);
+wsServer(wss);
 
 server.listen(7070, () => console.log("Server started"));
 
