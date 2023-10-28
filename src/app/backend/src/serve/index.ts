da23410cfdd30baa8a9107c64475a3e559835a29
Tryd0g0lik <work80@mail.ru>
2023-10-28 13:17:12 +0700

    [v5.2]: it's first commit

--- src/app/backend/src/serve/index.ts
+++ src/app/backend/src/serve/index.ts
@@ -15,11 +15,7 @@ const app = new Koa();
 const server = http.createServer(app.callback);
 const wss = new WS.Server({ server });
 const { appWebsockets: wsServer } = require('./wsServer');
-// app.use(async (ctx: any) => {
-// 	ctx.body = 'Hello World';
-// });
 
 wsServer(wss);
 
 server.listen(7070, () => console.log("Server started"));
-
